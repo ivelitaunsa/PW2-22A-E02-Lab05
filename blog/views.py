b9882255e89e83d842737992309966e1ef00a37b
Italo <ivelita@unsa.edu.pe>
2022-06-08 18:55:23 -0500

    Configurando post_list() para devolver los objetos

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+# Import the Post model
+from django.utils import timezone
+from blog.models import Post
 
 # Create your views here.
 #nueva funcion
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
